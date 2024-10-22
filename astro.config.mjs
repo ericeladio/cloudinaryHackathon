f14f080d04b4aa6b420702ac6148b9fb581ec89d
EricChavarria <eric.chavarria1409@gmail.com>
2024-10-22 09:17:07 -0600

    change adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     output:'server',
-    adapter: cloudflare(),
-
+    adapter: node({
+        mode: 'standalone', 
+      }),
 });
